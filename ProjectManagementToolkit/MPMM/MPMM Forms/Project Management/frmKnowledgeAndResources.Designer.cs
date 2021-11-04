ee99550f530947d5381ed8e88f06709087ddc06f
QJNel <quintinnel9@gmail.com>
2021-11-04 09:25:47 +0200

    Added Knowledge and Resources buttons

--- ProjectManagementToolkit/MPMM/MPMM Forms/Project Management/frmKnowledgeAndResources.Designer.cs
+++ ProjectManagementToolkit/MPMM/MPMM Forms/Project Management/frmKnowledgeAndResources.Designer.cs
@@ -29,12 +29,314 @@ namespace ProjectManagementToolkit.MPMM.MPMM_Forms.Project_Management
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel57 = new System.Windows.Forms.Panel();
+            this.label39 = new System.Windows.Forms.Label();
+            this.btnRiskManagement = new System.Windows.Forms.Button();
+            this.btnCostAndExpenseManagement = new System.Windows.Forms.Button();
+            this.btnChangeManagement = new System.Windows.Forms.Button();
+            this.btnCommunicationAndStakeholderManagement = new System.Windows.Forms.Button();
+            this.btnIssueManagement = new System.Windows.Forms.Button();
+            this.btnQualityManagement = new System.Windows.Forms.Button();
+            this.btnAcceptanceManagement = new System.Windows.Forms.Button();
+            this.btnProcurementManagement = new System.Windows.Forms.Button();
+            this.btnTimeAllocation = new System.Windows.Forms.Button();
+            this.btnExceptionManagement = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExceptionManagementST = new System.Windows.Forms.Button();
+            this.btnTimeAllocationST = new System.Windows.Forms.Button();
+            this.btnProcurementManagementST = new System.Windows.Forms.Button();
+            this.btnAcceptanceManagementST = new System.Windows.Forms.Button();
+            this.btnQualityManagementST = new System.Windows.Forms.Button();
+            this.btnIssueManagementST = new System.Windows.Forms.Button();
+            this.btnCommunicationAndStakeholderManagementST = new System.Windows.Forms.Button();
+            this.btnChangeManagementST = new System.Windows.Forms.Button();
+            this.btnCostAndExpenseManagementST = new System.Windows.Forms.Button();
+            this.btnRiskManagementST = new System.Windows.Forms.Button();
+            this.panel57.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel57
+            // 
+            this.panel57.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel57.Controls.Add(this.label39);
+            this.panel57.Location = new System.Drawing.Point(12, 12);
+            this.panel57.Name = "panel57";
+            this.panel57.Size = new System.Drawing.Size(521, 48);
+            this.panel57.TabIndex = 6;
+            // 
+            // label39
+            // 
+            this.label39.AutoSize = true;
+            this.label39.Font = new System.Drawing.Font("Cambria", 10F);
+            this.label39.Location = new System.Drawing.Point(189, 15);
+            this.label39.Name = "label39";
+            this.label39.Size = new System.Drawing.Size(88, 16);
+            this.label39.TabIndex = 5;
+            this.label39.Text = "Process Tools";
+            // 
+            // btnRiskManagement
+            // 
+            this.btnRiskManagement.Location = new System.Drawing.Point(12, 67);
+            this.btnRiskManagement.Name = "btnRiskManagement";
+            this.btnRiskManagement.Size = new System.Drawing.Size(142, 23);
+            this.btnRiskManagement.TabIndex = 8;
+            this.btnRiskManagement.Text = "Risk Management";
+            this.btnRiskManagement.UseVisualStyleBackColor = true;
+            // 
+            // btnCostAndExpenseManagement
+            // 
+            this.btnCostAndExpenseManagement.Location = new System.Drawing.Point(160, 66);
+            this.btnCostAndExpenseManagement.Name = "btnCostAndExpenseManagement";
+            this.btnCostAndExpenseManagement.Size = new System.Drawing.Size(235, 23);
+            this.btnCostAndExpenseManagement.TabIndex = 9;
+            this.btnCostAndExpenseManagement.Text = "Cost and Expense Management";
+            this.btnCostAndExpenseManagement.UseVisualStyleBackColor = true;
+            // 
+            // btnChangeManagement
+            // 
+            this.btnChangeManagement.Location = new System.Drawing.Point(402, 66);
+            this.btnChangeManagement.Name = "btnChangeManagement";
+            this.btnChangeManagement.Size = new System.Drawing.Size(131, 23);
+            this.btnChangeManagement.TabIndex = 10;
+            this.btnChangeManagement.Text = "Change Management";
+            this.btnChangeManagement.UseVisualStyleBackColor = true;
+            // 
+            // btnCommunicationAndStakeholderManagement
+            // 
+            this.btnCommunicationAndStakeholderManagement.Location = new System.Drawing.Point(12, 95);
+            this.btnCommunicationAndStakeholderManagement.Name = "btnCommunicationAndStakeholderManagement";
+            this.btnCommunicationAndStakeholderManagement.Size = new System.Drawing.Size(240, 23);
+            this.btnCommunicationAndStakeholderManagement.TabIndex = 11;
+            this.btnCommunicationAndStakeholderManagement.Text = "Communication And Stakeholder Management";
+            this.btnCommunicationAndStakeholderManagement.UseVisualStyleBackColor = true;
+            // 
+            // btnIssueManagement
+            // 
+            this.btnIssueManagement.Location = new System.Drawing.Point(258, 95);
+            this.btnIssueManagement.Name = "btnIssueManagement";
+            this.btnIssueManagement.Size = new System.Drawing.Size(137, 23);
+            this.btnIssueManagement.TabIndex = 12;
+            this.btnIssueManagement.Text = "Issue Management";
+            this.btnIssueManagement.UseVisualStyleBackColor = true;
+            // 
+            // btnQualityManagement
+            // 
+            this.btnQualityManagement.Location = new System.Drawing.Point(402, 95);
+            this.btnQualityManagement.Name = "btnQualityManagement";
+            this.btnQualityManagement.Size = new System.Drawing.Size(131, 23);
+            this.btnQualityManagement.TabIndex = 13;
+            this.btnQualityManagement.Text = "Quality Management";
+            this.btnQualityManagement.UseVisualStyleBackColor = true;
+            // 
+            // btnAcceptanceManagement
+            // 
+            this.btnAcceptanceManagement.Location = new System.Drawing.Point(13, 125);
+            this.btnAcceptanceManagement.Name = "btnAcceptanceManagement";
+            this.btnAcceptanceManagement.Size = new System.Drawing.Size(141, 23);
+            this.btnAcceptanceManagement.TabIndex = 14;
+            this.btnAcceptanceManagement.Text = "Acceptance Management";
+            this.btnAcceptanceManagement.UseVisualStyleBackColor = true;
+            // 
+            // btnProcurementManagement
+            // 
+            this.btnProcurementManagement.Location = new System.Drawing.Point(160, 124);
+            this.btnProcurementManagement.Name = "btnProcurementManagement";
+            this.btnProcurementManagement.Size = new System.Drawing.Size(141, 23);
+            this.btnProcurementManagement.TabIndex = 15;
+            this.btnProcurementManagement.Text = "Procurement Management";
+            this.btnProcurementManagement.UseVisualStyleBackColor = true;
+            // 
+            // btnTimeAllocation
+            // 
+            this.btnTimeAllocation.Location = new System.Drawing.Point(308, 124);
+            this.btnTimeAllocation.Name = "btnTimeAllocation";
+            this.btnTimeAllocation.Size = new System.Drawing.Size(87, 23);
+            this.btnTimeAllocation.TabIndex = 16;
+            this.btnTimeAllocation.Text = "Time Allocation";
+            this.btnTimeAllocation.UseVisualStyleBackColor = true;
+            // 
+            // btnExceptionManagement
+            // 
+            this.btnExceptionManagement.Location = new System.Drawing.Point(402, 124);
+            this.btnExceptionManagement.Name = "btnExceptionManagement";
+            this.btnExceptionManagement.Size = new System.Drawing.Size(131, 23);
+            this.btnExceptionManagement.TabIndex = 17;
+            this.btnExceptionManagement.Text = "Exception Management";
+            this.btnExceptionManagement.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(539, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(521, 48);
+            this.panel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 10F);
+            this.label1.Location = new System.Drawing.Point(189, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Stage Tools";
+            // 
+            // btnExceptionManagementST
+            // 
+            this.btnExceptionManagementST.Location = new System.Drawing.Point(929, 123);
+            this.btnExceptionManagementST.Name = "btnExceptionManagementST";
+            this.btnExceptionManagementST.Size = new System.Drawing.Size(131, 23);
+            this.btnExceptionManagementST.TabIndex = 27;
+            this.btnExceptionManagementST.Text = "Exception Management";
+            this.btnExceptionManagementST.UseVisualStyleBackColor = true;
+            // 
+            // btnTimeAllocationST
+            // 
+            this.btnTimeAllocationST.Location = new System.Drawing.Point(835, 123);
+            this.btnTimeAllocationST.Name = "btnTimeAllocationST";
+            this.btnTimeAllocationST.Size = new System.Drawing.Size(87, 23);
+            this.btnTimeAllocationST.TabIndex = 26;
+            this.btnTimeAllocationST.Text = "Time Allocation";
+            this.btnTimeAllocationST.UseVisualStyleBackColor = true;
+            // 
+            // btnProcurementManagementST
+            // 
+            this.btnProcurementManagementST.Location = new System.Drawing.Point(687, 123);
+            this.btnProcurementManagementST.Name = "btnProcurementManagementST";
+            this.btnProcurementManagementST.Size = new System.Drawing.Size(141, 23);
+            this.btnProcurementManagementST.TabIndex = 25;
+            this.btnProcurementManagementST.Text = "Procurement Management";
+            this.btnProcurementManagementST.UseVisualStyleBackColor = true;
+            // 
+            // btnAcceptanceManagementST
+            // 
+            this.btnAcceptanceManagementST.Location = new System.Drawing.Point(540, 124);
+            this.btnAcceptanceManagementST.Name = "btnAcceptanceManagementST";
+            this.btnAcceptanceManagementST.Size = new System.Drawing.Size(141, 23);
+            this.btnAcceptanceManagementST.TabIndex = 24;
+            this.btnAcceptanceManagementST.Text = "Acceptance Management";
+            this.btnAcceptanceManagementST.UseVisualStyleBackColor = true;
+            // 
+            // btnQualityManagementST
+            // 
+            this.btnQualityManagementST.Location = new System.Drawing.Point(929, 94);
+            this.btnQualityManagementST.Name = "btnQualityManagementST";
+            this.btnQualityManagementST.Size = new System.Drawing.Size(131, 23);
+            this.btnQualityManagementST.TabIndex = 23;
+            this.btnQualityManagementST.Text = "Quality Management";
+            this.btnQualityManagementST.UseVisualStyleBackColor = true;
+            // 
+            // btnIssueManagementST
+            // 
+            this.btnIssueManagementST.Location = new System.Drawing.Point(785, 94);
+            this.btnIssueManagementST.Name = "btnIssueManagementST";
+            this.btnIssueManagementST.Size = new System.Drawing.Size(137, 23);
+            this.btnIssueManagementST.TabIndex = 22;
+            this.btnIssueManagementST.Text = "Issue Management";
+            this.btnIssueManagementST.UseVisualStyleBackColor = true;
+            // 
+            // btnCommunicationAndStakeholderManagementST
+            // 
+            this.btnCommunicationAndStakeholderManagementST.Location = new System.Drawing.Point(539, 94);
+            this.btnCommunicationAndStakeholderManagementST.Name = "btnCommunicationAndStakeholderManagementST";
+            this.btnCommunicationAndStakeholderManagementST.Size = new System.Drawing.Size(240, 23);
+            this.btnCommunicationAndStakeholderManagementST.TabIndex = 21;
+            this.btnCommunicationAndStakeholderManagementST.Text = "Communication And Stakeholder Management";
+            this.btnCommunicationAndStakeholderManagementST.UseVisualStyleBackColor = true;
+            // 
+            // btnChangeManagementST
+            // 
+            this.btnChangeManagementST.Location = new System.Drawing.Point(929, 65);
+            this.btnChangeManagementST.Name = "btnChangeManagementST";
+            this.btnChangeManagementST.Size = new System.Drawing.Size(131, 23);
+            this.btnChangeManagementST.TabIndex = 20;
+            this.btnChangeManagementST.Text = "Change Management";
+            this.btnChangeManagementST.UseVisualStyleBackColor = true;
+            // 
+            // btnCostAndExpenseManagementST
+            // 
+            this.btnCostAndExpenseManagementST.Location = new System.Drawing.Point(687, 65);
+            this.btnCostAndExpenseManagementST.Name = "btnCostAndExpenseManagementST";
+            this.btnCostAndExpenseManagementST.Size = new System.Drawing.Size(235, 23);
+            this.btnCostAndExpenseManagementST.TabIndex = 19;
+            this.btnCostAndExpenseManagementST.Text = "Cost and Expense Management";
+            this.btnCostAndExpenseManagementST.UseVisualStyleBackColor = true;
+            // 
+            // btnRiskManagementST
+            // 
+            this.btnRiskManagementST.Location = new System.Drawing.Point(539, 67);
+            this.btnRiskManagementST.Name = "btnRiskManagementST";
+            this.btnRiskManagementST.Size = new System.Drawing.Size(142, 23);
+            this.btnRiskManagementST.TabIndex = 18;
+            this.btnRiskManagementST.Text = "Risk Management";
+            this.btnRiskManagementST.UseVisualStyleBackColor = true;
+            // 
+            // frmKnowledgeAndResources
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1084, 461);
+            this.Controls.Add(this.btnExceptionManagementST);
+            this.Controls.Add(this.btnTimeAllocationST);
+            this.Controls.Add(this.btnProcurementManagementST);
+            this.Controls.Add(this.btnAcceptanceManagementST);
+            this.Controls.Add(this.btnQualityManagementST);
+            this.Controls.Add(this.btnIssueManagementST);
+            this.Controls.Add(this.btnCommunicationAndStakeholderManagementST);
+            this.Controls.Add(this.btnChangeManagementST);
+            this.Controls.Add(this.btnCostAndExpenseManagementST);
+            this.Controls.Add(this.btnRiskManagementST);
+            this.Controls.Add(this.btnExceptionManagement);
+            this.Controls.Add(this.btnTimeAllocation);
+            this.Controls.Add(this.btnProcurementManagement);
+            this.Controls.Add(this.btnAcceptanceManagement);
+            this.Controls.Add(this.btnQualityManagement);
+            this.Controls.Add(this.btnIssueManagement);
+            this.Controls.Add(this.btnCommunicationAndStakeholderManagement);
+            this.Controls.Add(this.btnChangeManagement);
+            this.Controls.Add(this.btnCostAndExpenseManagement);
+            this.Controls.Add(this.btnRiskManagement);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel57);
+            this.Name = "frmKnowledgeAndResources";
             this.Text = "frmKnowledgeAndResources";
+            this.panel57.ResumeLayout(false);
+            this.panel57.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel57;
+        private System.Windows.Forms.Label label39;
+        private System.Windows.Forms.Button btnRiskManagement;
+        private System.Windows.Forms.Button btnCostAndExpenseManagement;
+        private System.Windows.Forms.Button btnChangeManagement;
+        private System.Windows.Forms.Button btnCommunicationAndStakeholderManagement;
+        private System.Windows.Forms.Button btnIssueManagement;
+        private System.Windows.Forms.Button btnQualityManagement;
+        private System.Windows.Forms.Button btnAcceptanceManagement;
+        private System.Windows.Forms.Button btnProcurementManagement;
+        private System.Windows.Forms.Button btnTimeAllocation;
+        private System.Windows.Forms.Button btnExceptionManagement;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExceptionManagementST;
+        private System.Windows.Forms.Button btnTimeAllocationST;
+        private System.Windows.Forms.Button btnProcurementManagementST;
+        private System.Windows.Forms.Button btnAcceptanceManagementST;
+        private System.Windows.Forms.Button btnQualityManagementST;
+        private System.Windows.Forms.Button btnIssueManagementST;
+        private System.Windows.Forms.Button btnCommunicationAndStakeholderManagementST;
+        private System.Windows.Forms.Button btnChangeManagementST;
+        private System.Windows.Forms.Button btnCostAndExpenseManagementST;
+        private System.Windows.Forms.Button btnRiskManagementST;
     }
 }
